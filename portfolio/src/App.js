d51dbdfc9c2f9f1e0ab1a65642be6c4d79f5ec5f
Liu Biyu <lby19981017@gmail.com>
2024-12-03 15:03:19 +0800

    updates and some color

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './pages/Home';
 import About from './components/About';
-import Experience from './components/Experience';
 import Education from './components/Education';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
@@ -14,9 +12,7 @@ function App() {
       <div className="app">
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/experience" element={<Experience />} />
+          <Route path="/" element={<About />} />
           <Route path="/education" element={<Education />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/skills" element={<Skills />} />
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -5,12 +5,9 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <h1>Your Name</h1>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/experience">Experience</Link></li>
           <li><Link to="/education">Education</Link></li>
           <li><Link to="/projects">Projects</Link></li>
           <li><Link to="/skills">Skills</Link></li>
