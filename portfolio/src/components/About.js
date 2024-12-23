c7ad7ccdf4788a072c1242916755889603ac6f5d
Liu Biyu <lby19981017@gmail.com>
2024-12-23 12:59:13 +0800

    update

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './css/Components.css'; // Make sure to include the CSS file
+import './css/About.css'; // Make sure to include the CSS file
 
 function About() {
     return (
@@ -8,8 +8,15 @@ function About() {
                 <h2>A LITTLE BIT ABOUT ME</h2>
                 <p>
                 Hi! My name is Liu Biyu, and I'm a web developer with a passion for 
-                front-end development and design. I graduated from the University of 
-                Wollongong with a degree in Computer Science (Big Data), which has 
+                front-end development and design. I graduated from the
+                {' '}<a href="https://www.uow.edu.au" target="_blank" rel="noopener noreferrer">
+                University of Wollongong
+                </a>{' '}
+                with a degree in 
+                {' '}<a href="https://www.sim.edu.sg/degrees-diplomas/programmes/programme-listing/bachelor-of-computer-science-big-data" target="_blank" rel="noopener noreferrer">
+                Computer Science (Big Data)
+                </a>
+                , which has 
                 provided me with a strong foundation in software development, 
                 data analysis, and modern web technologies.
                 I aspire to build a career where I can craft beautiful and 
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/Header.css';
+import throttle from 'lodash/throttle';
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = throttle(() => {
+      if (window.scrollY > 0 && !isScrolled) {
+        setIsScrolled(true);
+      } else if (window.scrollY === 0 && isScrolled) {
+        setIsScrolled(false);
+      }
+    }, 200);
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isScrolled]);
+
   return (
-    <header className="header">
+    <header className={`header ${isScrolled ? 'scrolled' : ''}`}>
       <div className="logo-container">
         <img src="" alt="Logo" />
         <span>LIU BIYU</span>
